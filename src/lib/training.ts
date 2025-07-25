21ffc86b41e20a2500737003246af33ac595709e
almtav08 <alexmartinez3107@gmail.com>
2025-07-25 09:58:46 +0200

    added training page

---
+++ src/lib/training.ts
@@ -0,0 +1,22 @@
+export const TRAINING = [
+  {
+    name: "Fairness and Interpretability in AI",
+    description:
+      "This course covers the principles of fairness and interpretability in AI systems, focusing on ethical considerations and practical applications.",
+    image: "fair.webp",
+    teachers: "Marina Martínez and Emilio Sansano",
+    startDate: "1-6-2025",
+    endDate: "31-7-2025",
+    place: "Banco Sabadell",
+  },
+  {
+    name: "Creating your own Chatbot",
+    description:
+      "This course teaches how to build chatbots using Large Language Models, covering both theoretical foundations and practical implementation.",
+    image: "chat.webp",
+    teachers: "Alex Martínez",
+    startDate: "31-1-2025",
+    endDate: "1-2-2025",
+    place: "Ayuntamiento de Onda",
+  }
+];
