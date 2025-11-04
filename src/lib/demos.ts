183ac076cb1a6985bb50af2387f5a4dc089ba387
almtav08 <alexmartinez3107@gmail.com>
2025-11-04 12:30:24 +0100

    added demo page

---
+++ src/lib/demos.ts
@@ -0,0 +1,22 @@
+export const DEMOS = [
+  /*{
+    name: "",
+    description: "",
+    link: "",
+    image: "",
+    }*/
+  {
+    name: "Adagio",
+    description:
+      "The Speech-to-Text application for persons with speech impairments.",
+    link: "https://ia3research.github.io",
+    image: "adagio.webp",
+  },
+  {
+    name: "Improven RAG",
+    description:
+      "Retrieval-Augmented Generation application to improve customer support.",
+    link: "https://ia3research.github.io",
+    image: "improven.webp",
+  },
+];
